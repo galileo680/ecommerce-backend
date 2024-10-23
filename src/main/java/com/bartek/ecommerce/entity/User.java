444c8ff62c05029ec382dd0a728abe672b69124d
galileo680 <bartek.wicz@gmail.com>
2024-10-23 22:27:57 +0200

    add entities

---
+++ src/main/java/com/bartek/ecommerce/entity/User.java
@@ -0,0 +1,52 @@
+package com.bartek.ecommerce.entity;
+
+import com.bartek.ecommerce.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Firstname is required")
+    private String firstname;
+
+    @NotBlank(message = "Lastname is required")
+    private String lastname;
+
+    @Column(unique = true)
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password number is required")
+    private String password;
+
+    @Column(name = "phone_number")
+    @NotBlank(message = "Phone number is required")
+    private  String phoneNumber;
+
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<OrderItem> orderItemList;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
+    private Address address;
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
