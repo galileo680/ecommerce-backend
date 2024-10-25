d3931d8638056b9af0f050a4b5cf7867c14cff7c
galileo680 <bartek.wicz@gmail.com>
2024-10-25 16:34:07 +0200

    fix UserDto fields

--- src/main/java/com/bartek/ecommerce/dto/UserDto.java
+++ src/main/java/com/bartek/ecommerce/dto/UserDto.java
@@ -3,12 +3,14 @@ package com.bartek.ecommerce.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
@@ -17,7 +19,8 @@ public class UserDto {
 
     private Long id;
     private String email;
-    private String name;
+    private String firstname;
+    private String lastname;
     private String phoneNumber;
     private String password;
     private String role;
