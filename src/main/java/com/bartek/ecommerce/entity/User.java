af894434a8e41df40b3584f86587d76edf106167
galileo680 <bartek.wicz@gmail.com>
2024-10-26 14:55:22 +0200

    add role entity

--- src/main/java/com/bartek/ecommerce/entity/User.java
+++ src/main/java/com/bartek/ecommerce/entity/User.java
@@ -7,9 +7,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Entity
@@ -17,29 +22,21 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank(message = "Firstname is required")
     private String firstname;
-
-    @NotBlank(message = "Lastname is required")
     private String lastname;
-
     @Column(unique = true)
-    @NotBlank(message = "Email is required")
     private String email;
-
-    @NotBlank(message = "Password number is required")
     private String password;
-
     @Column(name = "phone_number")
-    @NotBlank(message = "Phone number is required")
     private  String phoneNumber;
 
-    private UserRole role;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<OrderItem> orderItemList;
@@ -49,4 +46,42 @@ public class User {
 
     @Column(name = "created_at")
     private final LocalDateTime createdAt = LocalDateTime.now();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
