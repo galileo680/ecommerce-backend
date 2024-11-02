afa7527d5c366400fc8f533439ca5dcbd19cee3e
galileo680 <bartek.wicz@gmail.com>
2024-11-02 20:13:26 +0100

    add Aws service

---
+++ src/main/java/com/bartek/ecommerce/service/AwsS3Service.java
@@ -0,0 +1,57 @@
+package com.bartek.ecommerce.service;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+@Slf4j
+public class AwsS3Service {
+
+    private final String bucketName = "spring-ecommerce1";
+
+    @Value("${aws.s3.access}")
+    private String awsS3AccessKey;
+    @Value("${aws.s3.secrete}")
+    private String awsS3SecreteKey;
+
+
+    public String saveImageToS3(MultipartFile photo){
+        try {
+            String s3FileName = photo.getOriginalFilename();
+
+            BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsS3AccessKey, awsS3SecreteKey);
+
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                    .withRegion(Regions.US_EAST_2)
+                    .build();
+
+            InputStream inputStream = photo.getInputStream();
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("image/jpeg");
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, s3FileName, inputStream, metadata);
+            s3Client.putObject(putObjectRequest);
+
+            return "https://" + bucketName + ".s3.us-east-2.amazonaws.com/" + s3FileName;
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unable to upload image to s3 bucket: " + e.getMessage());
+        }
+    }
+}
