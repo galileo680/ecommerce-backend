f958b9280883e81890b2c43830aeea6b0c55af10
galileo680 <bartek.wicz@gmail.com>
2024-11-06 16:47:16 +0100

    add CartRepository change OrderItemRepository, OrderRepository

--- src/main/java/com/bartek/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/bartek/ecommerce/repository/OrderRepository.java
@@ -1,7 +1,12 @@
 package com.bartek.ecommerce.repository;
 
 import com.bartek.ecommerce.entity.Order;
+import com.bartek.ecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUser(User user);
 }
