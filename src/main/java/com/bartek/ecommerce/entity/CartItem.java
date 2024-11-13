10ed421bf62d9e3c759c2b7da3c2bc0211ac4ba9
galileo680 <bartek.wicz@gmail.com>
2024-11-13 22:34:37 +0100

    add ctors to CartItem entity

--- src/main/java/com/bartek/ecommerce/entity/CartItem.java
+++ src/main/java/com/bartek/ecommerce/entity/CartItem.java
@@ -1,13 +1,17 @@
 package com.bartek.ecommerce.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Entity
 @Data
 @Table(name = "cart_items")
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItem {
 
     @Id
@@ -24,4 +28,5 @@ public class CartItem {
     @ManyToOne
     @JoinColumn(name = "cart_id")
     private Cart cart;
+
 }
