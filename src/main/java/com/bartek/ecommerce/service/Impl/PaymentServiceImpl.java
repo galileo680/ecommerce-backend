2f3cda8dc7b4aa32cd58f6231d46d63514499c56
galileo680 <bartek.wicz@gmail.com>
2024-11-19 23:45:06 +0100

    add PaymentService

---
+++ src/main/java/com/bartek/ecommerce/service/Impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.bartek.ecommerce.service.Impl;
+
+import com.bartek.ecommerce.dto.PaymentDetailsDto;
+import com.bartek.ecommerce.entity.Order;
+import com.bartek.ecommerce.entity.Payment;
+import com.bartek.ecommerce.enums.PaymentStatus;
+import com.bartek.ecommerce.service.PaymentService;
+
+import java.time.LocalDateTime;
+
+public class PaymentServiceImpl implements PaymentService {
+
+    @Override
+    public Payment processPayment(Order order, PaymentDetailsDto paymentDetails) {
+
+        Payment payment = new Payment();
+        payment.setOrder(order);
+        payment.setMethod(paymentDetails.getPaymentMethod());
+        payment.setStatus(PaymentStatus.SUCCESS);
+        payment.setPaymentDate(LocalDateTime.now());
+        payment.setTransactionId("TXN123456789");
+
+        return payment;
+    }
+}
