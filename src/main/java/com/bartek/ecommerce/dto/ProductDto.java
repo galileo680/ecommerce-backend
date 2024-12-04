08c9553dec5af45c94e47a17aefa8a3ad1564a8f
galileo680 <bartek.wicz@gmail.com>
2024-12-04 23:06:59 +0100

    add validation constraints to ProductDto

--- src/main/java/com/bartek/ecommerce/dto/ProductDto.java
+++ src/main/java/com/bartek/ecommerce/dto/ProductDto.java
@@ -2,6 +2,7 @@ package com.bartek.ecommerce.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,11 +18,31 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDto {
+
     private Long id;
+
+    @NotBlank(message = "Product name is required")
+    @Size(min = 3, max = 15,  message = "Wrong name for a product")
     private String name;
+
+    @NotBlank(message = "Description is required")
+    @Size(min = 5, max = 50,  message = "Wrong description for a product")
     private String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "1.0", inclusive = false, message = "Price must be greater than 1.0")
     private BigDecimal price;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    @Max(value = 10000, message = "Quantity cannot exceed 10000")
+    private Integer quantity;
+
     private String imageUrl;
+
+    @NotNull(message = "Product image is required")
     private MultipartFile imageFile;
-    private CategoryDto category;
+
+    @NotNull(message = "Category ID is required")
+    private Long categoryId;
 }
