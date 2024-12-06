5b6efabe171e2274b22075579e209eef6c1f5367
galileo680 <bartek.wicz@gmail.com>
2024-12-06 18:52:24 +0100

    set orphanRemoval to true for cartItems

--- src/main/java/com/bartek/ecommerce/entity/Cart.java
+++ src/main/java/com/bartek/ecommerce/entity/Cart.java
@@ -21,6 +21,6 @@ public class Cart {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItem> cartItems;
 }
