999bd0f0a1f7ebd462c2fe286c7a46e2b013b5f2
galileo680 <bartek.wicz@gmail.com>
2024-12-08 18:04:04 +0100

    use AuthenticationPrincipal instead of getCurrentUserId

--- src/main/java/com/bartek/ecommerce/controller/OrderController.java
+++ src/main/java/com/bartek/ecommerce/controller/OrderController.java
@@ -2,12 +2,13 @@ package com.bartek.ecommerce.controller;
 
 import com.bartek.ecommerce.dto.OrderDto;
 import com.bartek.ecommerce.dto.OrderRequest;
+import com.bartek.ecommerce.entity.User;
 import com.bartek.ecommerce.service.OrderService;
-import com.bartek.ecommerce.util.SecurityUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,28 +24,33 @@ public class OrderController {
 
     @PostMapping("/checkout")
     public ResponseEntity<OrderDto> checkout(
+            @AuthenticationPrincipal User user,
             @RequestBody @Valid OrderRequest orderRequest
     ) {
-        Long userId = SecurityUtil.getCurrentUserId();
-        OrderDto orderDto = orderService.placeOrder(userId, orderRequest);
+        //Long userId = SecurityUtil.getCurrentUserId();
+
+        OrderDto orderDto = orderService.placeOrder(user.getId(), orderRequest);
 
         return new ResponseEntity<>(orderDto, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<OrderDto>> getUserOrders() {
-        Long userId = SecurityUtil.getCurrentUserId();
-        List<OrderDto> ordersDto = orderService.getOrdersByUserId(userId);
+    public ResponseEntity<List<OrderDto>> getUserOrders(
+            @AuthenticationPrincipal User user
+    ) {
+        //Long userId = SecurityUtil.getCurrentUserId();
+        List<OrderDto> ordersDto = orderService.getOrdersByUserId(user.getId());
 
         return ResponseEntity.ok(ordersDto);
     }
 
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderDto> getOrderById(
+            @AuthenticationPrincipal User user,
             @PathVariable Long orderId
     ) {
-        Long userId = SecurityUtil.getCurrentUserId();
-        OrderDto orderDto = orderService.getOrderById(orderId, userId);
+        //Long userId = SecurityUtil.getCurrentUserId();
+        OrderDto orderDto = orderService.getOrderById(orderId, user.getId());
 
         return ResponseEntity.ok(orderDto);
     }
