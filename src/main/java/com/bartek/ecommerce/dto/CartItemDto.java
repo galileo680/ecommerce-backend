123be22e8d35a6a27d895ce9081c5535b15da3ac
galileo680 <bartek.wicz@gmail.com>
2025-04-18 20:18:14 +0200

    add serialization to dtos

--- src/main/java/com/bartek/ecommerce/dto/CartItemDto.java
+++ src/main/java/com/bartek/ecommerce/dto/CartItemDto.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class CartItemDto {
+public class CartItemDto implements Serializable {
 
     private Long id;
     private Long productId;
