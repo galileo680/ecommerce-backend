54920adfb45bafd54cfcda0a46b7bbc99f31ca00
galileo680 <bartek.wicz@gmail.com>
2025-09-07 11:02:50 +0200

    init catalog-service

--- src/main/java/com/bartek/ecommerce/enums/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.bartek.ecommerce.enums;
-
-public enum PaymentStatus {
-    SUCCESS, DENIED
-}
--- src/main/java/com/bartek/ecommerce/exception/IllegalAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.bartek.ecommerce.exception;
-
-public class IllegalAccessException extends RuntimeException{
-    public IllegalAccessException(String message){
-        super(message);
-    }
-}
