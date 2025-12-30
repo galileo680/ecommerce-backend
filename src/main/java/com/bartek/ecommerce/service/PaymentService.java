244ccc7d095b7303fb3dbf5279111c302343997f
galileo680 <bartek.wicz@gmail.com>
2025-12-30 19:41:41 +0100

    remove

--- src/main/java/com/bartek/ecommerce/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.bartek.ecommerce.service;
-
-import com.bartek.ecommerce.dto.PaymentDetailsDto;
-import com.bartek.ecommerce.entity.Order;
-import com.bartek.ecommerce.entity.Payment;
-
-public interface PaymentService {
-    Payment processPayment(Order order, PaymentDetailsDto paymentDetailsDto);
-}
